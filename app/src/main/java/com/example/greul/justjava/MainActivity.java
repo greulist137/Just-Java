ce4d2dfaed1c4c9cbc83a47c995611e158968b41
Steven Greulich <greulich.steven@gmail.com>
2018-04-08 12:39:18 -0700

    commit - increment / decrement
    The purpose of this commit is that the increment / decrement for the quantity of coffees have been added.

--- app/src/main/java/com/example/greul/justjava/MainActivity.java
+++ app/src/main/java/com/example/greul/justjava/MainActivity.java
@@ -14,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -25,18 +27,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    int numberOfCoffees = 0;
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        displayPrice(numberOfCoffees * 5);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
+
+    public void increment(View view) {
+        numberOfCoffees++;
+        display(numberOfCoffees);
+    }
+
+    public void decrement(View view) {
+        if(numberOfCoffees != 0){
+            numberOfCoffees--;
+        }
+        display(numberOfCoffees);
+    }
+
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
